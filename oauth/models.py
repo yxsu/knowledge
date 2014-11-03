eb5f291024a051861a550501e9aa6e3b452415e9
Yuxin Su <suyuxin@outlook.com>
2014-11-03 19:19:47 +0800

    change to simple html5 form

--- oauth/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-# Create your models here.
-
-
-class AuthUser(models.Model):
-    access_token = models.CharField(max_length=300)
-    user_name = models.CharField(max_length=30)
-    time = models.DateTimeField()
